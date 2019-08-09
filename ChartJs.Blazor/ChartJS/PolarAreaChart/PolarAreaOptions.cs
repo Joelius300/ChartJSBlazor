bf9c32ff89cb01ddcd46e950a3bc6150821fb38a
SeppPenner <HansM2013@gmx.de>
2019-08-09 11:09:32 +0200

    Reworked polar area chart.

--- ChartJs.Blazor/ChartJS/PolarAreaChart/PolarAreaOptions.cs
+++ ChartJs.Blazor/ChartJS/PolarAreaChart/PolarAreaOptions.cs
@@ -4,15 +4,19 @@ using ChartJs.Blazor.ChartJS.Common.Properties;
 
 namespace ChartJs.Blazor.ChartJS.PolarAreaChart
 {
+    /// <summary>
+    /// The options-subconfig of a <see cref="PolarAreaChartConfig"/>
+    /// </summary>
     public class PolarAreaOptions : BaseChartConfigOptions
     {
         /// <summary>
-        /// Starting angle to draw arcs for the first item in a dataset.
+        /// Gets or sets the starting angle to draw arcs for the first item in a dataset.
         /// </summary>
         public double StartAngle { get; set; } = -0.5 * Math.PI;
 
+        /// <summary>
+        /// Gets or sets the animation the chart uses.
+        /// </summary>
         public PieDoughnutAnimation Animation { get; set; }
-
-        public PolarAreaScale Scale { get; set; }
     }
 }
