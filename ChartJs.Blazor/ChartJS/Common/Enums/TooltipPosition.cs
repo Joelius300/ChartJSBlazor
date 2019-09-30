1c3f9b3f9f42f62bf3420117c73c6ad23ece0976
SeppPenner <HansM2013@gmx.de>
2019-09-30 11:20:09 +0200

    Added tooltips stuff from @TomaszGrzmilas.

---
+++ ChartJs.Blazor/ChartJS/Common/Enums/TooltipPosition.cs
@@ -0,0 +1,25 @@
+﻿namespace ChartJs.Blazor.ChartJS.Common.Enums
+{
+    /// <summary>
+    /// Specifies position of the tooltips in the chart.
+    /// <para>As per documentation here https://www.chartjs.org/docs/latest/configuration/tooltip.html#position-modes </para>
+    /// </summary>
+    public sealed class TooltipPosition : StringEnum
+    {
+        /// <summary>
+        /// When <see cref="TooltipPosition.Average" /> is set, the tooltips will be placed at the average position of the items displayed in the tooltip.
+        /// </summary>
+        public static TooltipPosition Average => new TooltipPosition("average");
+
+        /// <summary>
+        /// When <see cref="TooltipPosition.Nearest" /> is set, the tooltips will be placed at the position of the element closest to the event position.
+        /// </summary>
+        public static TooltipPosition Nearest => new TooltipPosition("nearest");
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="TooltipPosition"/> class.
+        /// </summary>
+        /// <param name="stringValue">The <see cref="string"/> value to set.</param>
+        private TooltipPosition(string stringValue) : base(stringValue) { }
+    }
+}
